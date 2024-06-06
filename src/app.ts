25dbe0be46623a8f642050560be6e4d4b4829e55
242zayn <sarveshsharma@gmail.com>
2024-06-06 15:41:55 +0530

    create user model

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { json, NextFunction, Request, Response } from "express";
 
 import globleErrorHandler from "./middleware/GlobleErrorHandler";
 import userRouter from "./user/router";
 
 const app = express();
+app.use(json());
 app.use(globleErrorHandler);
 app.use("/api/users", userRouter);
 
--- src/user/userControler.ts
+++ src/user/userControler.ts
@@ -1,10 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import userScheema from "./userModel";
 
-const createUser =async (req : Request , res:Response , next: NextFunction)=>{
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
 
-   return res.json({
-        message: "User Seccesfully Ragisterd",
-      });
-}
+  if (!name || !email || !password) {
+    const err = createHttpError(400, "All feild are required ");
+    return next(err);
+  }
 
-export { createUser}
+  const user = await userScheema.findOne({ email: email });
+  if (user) {
+    const error = createHttpError(400, "User alrady exist");
+    return next(error)
+  }
+};
+
+export { createUser };
