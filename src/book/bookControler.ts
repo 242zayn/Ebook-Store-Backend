f962a6dc2ae99f0625de3a17fa6ee5c5df3924ff
242zayn <sarveshsharma@gmail.com>
2024-06-08 11:40:57 +0530

    create book controler

--- src/book/bookControler.ts
+++ src/book/bookControler.ts
@@ -1,7 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import path from "node:path";
+import cloudinary from "../config/cloudinary";
 
-const createbook = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, price } = req.body;
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.files);
+
+  const files = req.files as { [filename: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+  const uploadsResult = await cloudinary.uploader.upload(filePath, {
+    filename_override: fileName,
+    folder: "book-cover",
+    format: coverImageMimeType,
+  });
+  console.log("upload result", uploadsResult);
+  res.json({});
 };
 
-export { createbook };
+export { createBook };
